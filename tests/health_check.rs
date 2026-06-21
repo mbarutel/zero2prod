f8a5d86c91ca84e096d80218ff898da55e9e5cf7
Michel Philippe Edubas <mikeedubas@hotmail.com>
2026-06-21 17:36:13 +1000

    starting page 78

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,7 +1,8 @@
 use std::net::TcpListener;
 
-use sqlx::PgPool;
-use zero2prod::configuration::get_configuration;
+use sqlx::{AssertSqlSafe, Connection, Executor, PgConnection, PgPool};
+use uuid::Uuid;
+use zero2prod::configuration::{DatabaseSetting, get_configuration};
 
 #[tokio::test]
 async fn health_check_works() {
@@ -94,10 +95,9 @@ async fn spawn_app() -> TestApp {
     let port = listener.local_addr().unwrap().port();
     let address = format!("http://127.0.0.1:{}", port);
 
-    let configuration = get_configuration().expect("Failed to read configuration.");
-    let db_pool = PgPool::connect(&configuration.database.connection_string())
-        .await
-        .expect("Failed to connect to Postgres.");
+    let mut configuration = get_configuration().expect("Failed to read configuration.");
+    configuration.database.database_name = Uuid::new_v4().to_string();
+    let db_pool = configure_database(&configuration.database).await;
     let server =
         zero2prod::startup::run(listener, db_pool.clone()).expect("Failed to bind address");
 
@@ -107,4 +107,28 @@ async fn spawn_app() -> TestApp {
     let _ = tokio::spawn(server);
 
     TestApp { address, db_pool }
+}
+
+pub async fn configure_database(config: &DatabaseSetting) -> PgPool {
+    // Create database
+    let mut connection = PgConnection::connect(&config.connection_string_without_db())
+        .await
+        .expect("Failed to connect to Postgres.");
+    let query = format!(r#"CREATE DATABASE "{}";"#, config.database_name);
+
+    sqlx::raw_sql(AssertSqlSafe(query))
+        .execute(&mut connection)
+        .await
+        .expect("Failed to create database.");
+
+    // Migrate database
+    let connection_pool = PgPool::connect(&config.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
+    sqlx::migrate!("./migrations")
+        .run(&connection_pool)
+        .await
+        .expect("Failed to migrate the database.");
+
+    connection_pool
 }
