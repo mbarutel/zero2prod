4446bb6e5dc2dc72e2dd4b360627074a53ea2ca7
Mike Edubas <mike.edubas@westpac.com.au>
2026-07-25 15:56:57 +1000

    logging completed and now time to go live!

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,4 +1,5 @@
 use once_cell::sync::Lazy;
+use secrecy::ExposeSecret;
 use sqlx::{AssertSqlSafe, Connection, PgConnection, PgPool};
 use std::net::TcpListener;
 use uuid::Uuid;
@@ -135,9 +136,10 @@ async fn spawn_app() -> TestApp {
 
 pub async fn configure_database(config: &DatabaseSetting) -> PgPool {
     // Create database
-    let mut connection = PgConnection::connect(&config.connection_string_without_db())
-        .await
-        .expect("Failed to connect to Postgres.");
+    let mut connection =
+        PgConnection::connect(&config.connection_string_without_db().expose_secret())
+            .await
+            .expect("Failed to connect to Postgres.");
     let query = format!(r#"CREATE DATABASE "{}";"#, config.database_name);
 
     sqlx::raw_sql(AssertSqlSafe(query))
@@ -146,7 +148,7 @@ pub async fn configure_database(config: &DatabaseSetting) -> PgPool {
         .expect("Failed to create database.");
 
     // Migrate database
-    let connection_pool = PgPool::connect(&config.connection_string())
+    let connection_pool = PgPool::connect(&config.connection_string().expose_secret())
         .await
         .expect("Failed to connect to Postgres.");
     sqlx::migrate!("./migrations")
